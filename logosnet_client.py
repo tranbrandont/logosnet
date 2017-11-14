fb828094ae153fbc14474e8fb0a2f71f51a62a88
tranb7 <tranb7@wwu.edu>
2017-11-14 15:17:49 -0800

    created client and server

---
+++ logosnet_client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python           # This is server.py file
+"""This runs a client for the chatting application"""
+import socket
+import argparse
+import struct
+
+SOCK_LIST  = []
+
+def chat_client(port, ipnum):
+    sock=socket.socket()
+    if ipnum is None:
+        ipnum = socket.gethostname()
+    sock.connect((ipnum, port))
+    while 1:
+        message = input()
+        message = bytes(message, 'utf-8')
+        message = struct.pack('!s', message)
+        sock.send(message)
+
+
+
+
+def main():
+    """Parses command line arguments, starts client"""
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--port', type=int, required=True, help='port number')
+    parser.add_argument('--ip', help='IP address for client')
+    args = parser.parse_args()
+    port = args.port
+    ipnum = args.ip
+    chat_client(port, ipnum)
+
+
+
+main()
---
+++ logosnet_server.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python           # This is server.py file
+"""This runs a client for the chatting application"""
+import socket
+import argparse
+import select
+import struct
+
+SOCK_LIST  = []
+
+def chat_server(port, ipnum):
+    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Server started")
+    if ipnum is None:
+        ipnum = socket.gethostname()
+    serv_sock.bind((ipnum, port))
+    serv_sock.listen(100)
+    SOCK_LIST.append(serv_sock)
+
+    while 1:
+        read, write, error = select.select(SOCK_LIST, [], [], 0)
+        for sock in read:
+            if sock == serv_sock:
+                con, _addr = serv_sock.accept()
+                SOCK_LIST.append(con)
+                print("client connected (%s, %s)" % _addr)
+                broadcast(serv_sock, con, "[%s:%s] entered our chatting room\n" %_addr)
+            else:
+                try:
+                    data = sock.recv(10000)
+                    if data:
+                        data = struct.unpack('!s', data)
+                        data = data[0].decode("utf-8")
+                        print(data)
+                        broadcast(serv_sock, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
+                    else:
+                        if sock in SOCK_LIST:
+                            SOCK_LIST.remove(sock)
+                            broadcast(serv_sock, sock, "Client (%s, %s) is offline\n" % _addr)
+                except:
+                    broadcast(serv_sock, sock, "Client (%s, %s) is offline\n" % _addr)
+                    continue
+    serv_sock.close()
+
+
+
+
+def broadcast(serv_sock, sock, message):
+    for sockpeer in SOCK_LIST:
+        if sockpeer != serv_sock and sockpeer != sock :
+            try:
+                sockpeer.send(message)
+            except:
+                sockpeer.close()
+                if sockpeer in SOCK_LIST:
+                    SOCK_LIST.remove(sockpeer)
+
+
+
+
+def main():
+    """Parses command line arguments, starts client"""
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--port', type=int, required=True, help='port number')
+    parser.add_argument('--ip', help='IP address for client')
+    args = parser.parse_args()
+    port = args.port
+    ipnum = args.ip
+    chat_server(port, ipnum)
+
+
+main()
